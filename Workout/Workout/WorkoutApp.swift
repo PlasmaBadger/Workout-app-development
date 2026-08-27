b5d2b00f78db635965ec39dc6c32ec398e81edee
sredd <sredd@wisc.edu>
2026-08-27 16:18:46 -0500

    inital commit

---
+++ Workout/Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Sophie Redd on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
